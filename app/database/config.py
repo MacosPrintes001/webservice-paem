8699d858092fa143adc8ed812997e676f81adfcc
alfser <j.janilson12@gmail.com>
2021-06-14 17:54:36 -0300

    [feat] refatoring code of credentials of app.

---
+++ app/database/config.py
@@ -0,0 +1,53 @@
+import json
+import os
+import sys
+
+if os.path.isfile('/etc/config.json'):
+    with open('/etc/config.json') as file:
+        try:
+            __conn_json = json.load(file)
+
+        except json.decoder.JSONDecodeError as msg:
+            print("Erro ao decodificar o arquivo json.", msg)
+
+    username = __conn_json.get("USERNAME")
+    password = __conn_json.get("PASSWORD")
+    server = __conn_json.get("HOSTNAME")
+    database = __conn_json.get("DATABASE")
+    secret_key= __conn_json.get("SECRET_KEY")
+
+elif os.path.isfile('app/database/connection.json'):
+    with open('app/database/connection.json') as file:    
+        try:
+            __conn_json = json.load(file)
+
+        except json.decoder.JSONDecodeError as msg:
+                print("Erro ao decodificar o arquivo json.", msg)
+
+    username = __conn_json.get("USERNAME")
+    password = __conn_json.get("PASSWORD")
+    server = __conn_json.get("HOSTNAME")
+    database = __conn_json.get("DATABASE")
+    secret_key= __conn_json.get("SECRET_KEY")
+else:
+    
+    username = os.environ.get("USERNAME")
+    print(username)
+    password = os.environ.get("PASSWORD")
+    print(password)
+    server = os.environ.get("HOSTNAME")
+    print(server)
+    database = os.environ.get("DATABASE")
+    print(database)
+    secret_key = os.environ.get("SECRET_KEY")
+
+if not (username and server and password and database):
+    print("Erro: Não pode haver credênciais nulas.")
+    sys.exit()
+
+class Config:
+    USERNAME = username
+    HOSTNAME = server
+    DATABASE = database
+    SECRET_KEY = secret_key
+    PASSWORD = password
--- app/database/db.py
+++ app/database/db.py
@@ -1,8 +1,9 @@
 # Create a default database or recreate if it exist
 import sys
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+from flask import Flask, config
 from mysql import connector
+from .config import Config
 import json
 import os
 
@@ -10,78 +11,38 @@ import os
 app = Flask(__name__)
 
 
-if os.path.isfile('../connection.json'):
-    with open('../connection.json', 'r') as file:
-        try:
-            __conn_json = json.load(file)
-
-        except json.decoder.JSONDecodeError as msg:
-            print("Erro ao decodificar o arquivo json.", msg)
-
-    __username = __conn_json.get("username")
-    __password = __conn_json.get("password")
-    __server = __conn_json.get("server")
-    __database = __conn_json.get("database")
-
-elif os.path.isfile('app/database/connection.json'):
-    with open('app/database/connection.json') as file:    
-        try:
-            __conn_json = json.load(file)
-
-        except json.decoder.JSONDecodeError as msg:
-                print("Erro ao decodificar o arquivo json.", msg)
-
-    __username = __conn_json.get("username")
-    __password = __conn_json.get("password")
-    __server = __conn_json.get("server")
-    __database = __conn_json.get("database")
-
-else:
-    
-    __username = os.environ.get("USER_NAME")
-    print(__username)
-    __password = os.environ.get("PASSWORD")
-    print(__password)
-    __server = os.environ.get("HOST_NAME")
-    print(__server)
-    __database = os.environ.get("DATABASE_NAME")
-    print(__database)
 
 # get AQLAlchemy
 db = SQLAlchemy(app=app)
 
 __str_connection = "mysql://{username}:{password}@{server}/{database}?charset=utf8"
 
-if not (__username or __server or __password or __database):
-    print("Erro: Não pode haver credênciais nulas.")
-    sys.exit()
+config = Config()
 
+app.config['SECRET_KEY'] = Config.SECRET_KEY
+
+# app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = __str_connection.format(
-                                                    username=__username, 
-                                                    password=__password, 
-                                                    server=__server, 
-                                                    database=__database
+                                                    username= Config.USERNAME, 
+                                                    password= Config.PASSWORD, 
+                                                    server=Config.HOSTNAME, 
+                                                    database=Config.DATABASE
                                                 )
-
-app.config['SECRET_KEY'] = 'secrect_key'
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 def create_db():
     
     mydb = connector.connect(
-        host=__server,
-        user=__username,
-        password=__password
+        host=Config.HOSTNAME,
+        user=Config.USERNAME,
+        password=Config.PASSWORD
     )
     mycursor = mydb.cursor()
 
-    mycursor.execute(f"DROP DATABASE IF EXISTS {__database}")
-    mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {__database} CHARSET = utf8mb4;")
-    # mycursor.execute(f"USE {db_name}")
+    mycursor.execute(f"DROP DATABASE IF EXISTS {Config.DATABASE}")
+    mycursor.execute(f"CREATE DATABASE {Config.DATABASE} CHARSET = utf8;")
     mydb.close()
     mycursor.close()
     
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.create_all()
     
