50d56f007fa4e394655675bc428157f726599c21
alfser <j.janilson12@gmail.com>
2021-06-19 23:42:24 -0300

    adding route in home

--- app/resources/home.py
+++ app/resources/home.py
@@ -15,6 +15,7 @@ class HomeResource(Resource):
                 "discente": ["/discentes", "/discentes/discente"],
                 "docente": ["/docentes", "/docentes/docente"],
                 "tecnico": ["/tecnicos", "/tecnicos/tecnico"],
+                "curso": ["/cursos", "/cursos/curso"],
                 "campus": ["/campus", "/campus/campi"],
                 "recurso_campus": ["/recursos_campus", "/recursos_campus/recurso_campus"],
                 "solicitacao_acesso": ["/solicitacoes_acessos", "/solicitacoes_acessos/solicitacao_acesso"],
